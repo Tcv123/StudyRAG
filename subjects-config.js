ff83432216751319b3f99cf6e2eeb0717928b9ea
Tharun <tharun@Tharuns-Macbook-Air.local>
2026-09-14 14:19:44 +0100

    Auto-push: 2026-09-14 14:19

--- subjects-config.js
+++ subjects-config.js
@@ -44,7 +44,7 @@
     'Psychology':          ['AQA', 'Edexcel', 'OCR'],
     'Sociology':           ['AQA', 'OCR', 'Eduqas'],
     'Business Studies':    ['AQA', 'Edexcel', 'OCR', 'Eduqas'],
-    'Politics':            ['AQA', 'Edexcel', 'Eduqas'],
+    'Politics':            ['AQA'],
     'French':              ['AQA', 'Edexcel', 'Eduqas'],
     'German':              ['AQA', 'Edexcel', 'Eduqas'],
     'Spanish':             ['AQA', 'Edexcel', 'Eduqas'],
